b992a1fee916c6a67261dcfc37cce62b0a7d1db6
Martin Trnka <martin.trnka@partnersgroup.sk>
2023-04-21 09:57:57 +0200

    add migration for salesman

--- database/migrations/2023_04_18_152632_create_salesmen_table.php
+++ database/migrations/2023_04_18_152632_create_salesmen_table.php
@@ -13,12 +13,22 @@ return new class extends Migration
     {
         Schema::create('salesmen', function (Blueprint $table) {
             $table->id();
+            $table->uuid();
             $table->string('first_name', 50);
             $table->string('last_name', 50);
             $table->enum('titles_before', [
                 'Bc.', 'Mgr.', 'Ing.', 'JUDr.', 'MVDr.', 'MUDr.', 'PaedDr.', 'prof.', 'doc.', 'dipl.', 'MDDr.', 'Dr.',
                 'Mgr. art.', 'ThLic.', 'PhDr.', 'PhMr.', 'RNDr.', 'ThDr.', 'RSDr.', 'arch.', 'PharmDr.'
-            ]);
+            ])->nullable();
+            $table->enum('titles_after', [
+                'CSc.', 'DrSc.', 'PhD.', 'ArtD.', 'DiS', 'DiS.art', 'FEBO', 'MPH', 'BSBA', 'MBA', 'DBA', 'MHA',
+                'FCCA', 'MSc.', 'FEBU', 'LL.M'
+            ])->nullable();
+            $table->string('prosight_id', 5)->unique();
+            $table->string('email', 150)->unique();
+            $table->string('phone', 150)->nullable();
+            $table->enum('gender', ['m', 'f']);
+            $table->enum('marital_status', ['single', 'married', 'divorced', 'widowed']);
             $table->timestamps();
         });
     }
